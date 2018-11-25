106c308a5664abbcd8168b3dd9d1aa30b141fa1c
vithyaepsi <vithya.voeuk@epsi.fr>
2018-11-25 17:21:42 +0100

    security in memory

---
+++ src/main/java/fr/epsi/POEI/PapoteCar/conf/WebConfig.java
@@ -0,0 +1,30 @@
+package fr.epsi.POEI.PapoteCar.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	public void configureGlobal( AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("prout").password("{noop}caca").roles("USER", "USER_API", "ADMIN")
+			.and()
+			.withUser("api").password("{noop}api").roles("USER_API");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/**").hasAnyRole("ADMIN", "USER").and()
+			.httpBasic()
+			.and()
+			.csrf().disable();
+	}
+		
+}
