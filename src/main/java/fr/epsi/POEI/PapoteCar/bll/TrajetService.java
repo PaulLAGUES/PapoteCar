65423a6f3b2d0c4523060a7bf1502c48d57f87f9
ronron <vithya.voeuk@epsi.fr>
2018-12-18 09:46:38 +0100

    fichier oublié

--- src/main/java/fr/epsi/POEI/PapoteCar/bll/TrajetService.java
+++ src/main/java/fr/epsi/POEI/PapoteCar/bll/TrajetService.java
@@ -1,5 +1,17 @@
 package fr.epsi.POEI.PapoteCar.bll;
 
-public class TrajetService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.epsi.POEI.PapoteCar.dal.TrajetRepository;
+import fr.epsi.POEI.PapoteCar.domain.Trajet;
+
+public class TrajetService {
+	@Autowired 
+	TrajetRepository trajetRepository;
+	
+	public List<Trajet> findNearbyTrajetsByDestination(double posLat, double posLong, double destLat, double destLong){
+		GPSUtils.getGPSBounds()
+	}
 }
